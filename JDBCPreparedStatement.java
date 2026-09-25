a597c4cf812b2424eb722bdd65d7290e43d57670
23r01a04g3 <munimokshamshivareddy@gmail.com>
2026-09-25 20:05:34 +0530

    Update JDBCPreparedStatement.java

--- JDBCPreparedStatement.java
+++ JDBCPreparedStatement.java
@@ -18,7 +18,7 @@ public class JDBCPreparedStatement {
 		String URL = "jdbc:mysql://localhost:3306/JDBCMySQL";
 		
 		String Username = "root";
-		String Passward = "Venky@007";
+		String Passward = "Shiva@011";
 		
 		String Query = "SELECT * FROM Students WHERE Name = ?";
 		
